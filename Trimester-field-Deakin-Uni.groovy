244aaeb1a09603d0ce3eef54c3f2ff8395d0714b
adamst85 <straube.adam@gmail.com>
2016-02-03 14:45:06 +1100

    added 2016 trimester dates

--- Trimester-field-Deakin-Uni.groovy
+++ Trimester-field-Deakin-Uni.groovy
@@ -16,7 +16,7 @@ import org.joda.time.format.*
  
 timestamp = issue.getCreated()
  
-DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.S")
+DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS")
 DateTime dtIssue1 = formatter.parseDateTime((String)timestamp)
 LocalDate dtIssue = dtIssue1.toLocalDate();
     
@@ -159,8 +159,8 @@ switch(year) {
   case 2015:
   
     endT3d = new DateMidnight((int)year, 2, 20);
-   	startT1d = new DateMidnight((int)year, 3, 10);
- 	endT1d = new DateMidnight((int)year, 6, 20);
+   	startT1d = new DateMidnight((int)year, 3, 9);
+ 	endT1d = new DateMidnight((int)year, 6, 19);
     	startT2d = new DateMidnight((int)year, 7, 14);
  	endT2d = new DateMidnight((int)year, 10, 24);
  	startT3d = new DateMidnight((int)year, 11, 10);   
@@ -199,22 +199,69 @@ switch(year) {
      		break;
         }
         
-        case 2016:
+  case 2016:
 		
-			endT3d = new DateMidnight((int)year, 2, 19);
- 
-			LocalDate endT3 = endT3d.toLocalDate();
+	endT3d = new DateMidnight((int)year, 2, 19);
+        startT1d = new DateMidnight((int)year, 3, 7);
+        endT1d = new DateMidnight((int)year, 6, 17);
+        startT2d = new DateMidnight((int)year, 7, 11);
+        endT2d = new DateMidnight((int)year, 10, 21);
+        startT3d = new DateMidnight((int)year, 11, 7);
+
+
+        LocalDate startT1 = startT1d.toLocalDate();
+        LocalDate endT1 = endT1d.toLocalDate();
+        LocalDate startT2 = startT2d.toLocalDate();
+        LocalDate endT2 = endT2d.toLocalDate();
+        LocalDate startT3 = startT3d.toLocalDate();
+        LocalDate endT3 = endT3d.toLocalDate();
+
 		
         if (endT3.isAfter(dtIssue))
         {
   		trimesterString = 'T3-2015';
         	break;
         }
+        else if ((startT1.isBefore(dtIssue)) && (endT1.isAfter(dtIssue)))
+        {
+                trimesterString = 'T1-'+String.valueOf((int)year);
+                break;
+        }
+        else if ((startT2.isBefore(dtIssue)) && (endT2.isAfter(dtIssue)))
+        {
+                trimesterString = 'T2-'+String.valueOf((int)year);
+                break;
+        }
+        else if (startT3.isBefore(dtIssue))
+        {
+                trimesterString = 'T3-'+String.valueOf((int)year);
+                break;
+        }
         else
         {
         	trimesterString = 'none';
      		break;
         }
+
+  case 2017:
+
+        endT3d = new DateMidnight((int)year, 2, 17);
+
+        LocalDate endT3 = endT3d.toLocalDate();
+
+        if (endT3.isAfter(dtIssue))
+        {
+                trimesterString = 'T3-2016';
+                break;
+        }
+        else
+        {
+                trimesterString = 'none';
+                break;
+        }
+
+
+
  
 }
 return trimesterString
