eee30dff02cb9b936a63cf12991263e87a89afdb
adamst85 <straube.adam@gmail.com>
2016-05-19 16:36:35 +1000

    Updated Issue-updated-custom-listener.groovy comments section

--- Issue-updated-custom-listener.groovy
+++ Issue-updated-custom-listener.groovy
@@ -1,3 +1,11 @@
+// Issue updated custom listener script
+//
+// Used to stop notifications from changes to the labels field (still sends notifications for all other fields)
+//
+// Listener type: Fires an event when condition is true
+// Events: Issue Updated
+
+
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
  
